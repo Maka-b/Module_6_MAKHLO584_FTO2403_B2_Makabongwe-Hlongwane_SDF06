ee69c33f8ab7a07df324d6c0e946cea5208083d1
Makabongwe Hlongwane <jabumak001@gmail.com>
2024-04-01 18:36:40 +0200

    javascript file added

--- index.js
+++ index.js
@@ -1,13 +1,94 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
+//imported functions 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, remove, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
+//created an object (appSettings) and set its value to my Firebase database
+const appSettings = {
+    databaseURL : "https://realtime-database-24b37-default-rtdb.firebaseio.com/"
+}
+//
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+//imported reference function
+const shoppingListInDB = ref(database, "shoppingList")
+//
 const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+const addbtnEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+addbtnEl.addEventListener("click", function() {
+    //Checks if anything has been added to the string
+    if (inputFieldEl.value) {
+        let inputFieldEntry = inputFieldEl.value
+    // function pushes the input value to the database
+        push(shoppingListInDB, inputFieldEntry)
+
+        //console.log(inputFieldEntry)
+     
+        clearBar()
+    } else {
+        nothingAdded()
+    }
     
-    console.log(inputValue)
+
+    
 })
+//whenever there is change to the database
+onValue (shoppingListInDB, function(snapshot) {
+  //checks to see if there are items in the database
+    if (snapshot.exists()) {
+       let itemsArray = Object.entries(snapshot.val())
+    // we want the snapshot values as array, but paired to their unique keys 
+       clearShoppingListEl()
+    
+       //console.log(itemsArray)
+        //appends each item to the shopping list
+        for (let i = 0; i < itemsArray.length; i++) {
+
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            addToShoppingList(currentItem)
+        } 
+    }else {
+        shoppingListEl.innerHTML = "no items yet..."
+    }
+
+    
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+//clears user input bar
+function clearBar() {
+    inputFieldEl.value = ""
+    inputFieldEl.placeholder = "add some more!"
+}
+//adds (+-) inputed values as a list in HTML
+function addToShoppingList(item) {
+    //shoppingListEl.innerHTML += `<li>${input}</li>`
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+
+    newEl.textContent = itemValue
+
+   // shoppingListEl.append(newEl)
+
+    // Delete function
+    newEl.addEventListener("dblclick", function() {
+        // gets the location of the dblclicked item
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+        
+        remove(exactLocationOfItemInDB)
+    })
+
+    shoppingListEl.append(newEl)
+}
+
+function nothingAdded() {
+  document.getElementById("input-field").placeholder = "Nothing added yet..."
+  
+}
